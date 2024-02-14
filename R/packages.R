5f9b8986207b996590a9630dbdea1f108b17a0fa
Alec L. Robitaille <robit.alec@gmail.com>
2024-02-14 16:16:29 -0400

    resolve conflicts

--- R/packages.R
+++ R/packages.R
@@ -6,7 +6,11 @@ library(targets)
 library(tarchetypes)
 library(qs)
 
-# library(conflicted)
+library(conflicted)
+conflict_prefer_all('brms', quiet = TRUE)
+conflict_prefer_all('tidybayes', quiet = TRUE)
+conflict_prefer_all('posterior', quiet = TRUE)
+conflict_prefer_all('emmeans', quiet = TRUE)
 
 library(data.table)
 
@@ -23,6 +27,7 @@ theme_set(theme_bw())
 
 library(knitr)
 
+library(rstan)
 library(zarg)
 library(cmdstanr)
 library(rethinking)
@@ -36,8 +41,10 @@ library(janitor)
 
 
 
+
 # Installs ----------------------------------------------------------------
 # Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1)
 # install.packages("V8")
 # install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 # devtools::install_github("rmcelreath/rethinking")
+# remotes::install_github('robitalec/zarg')
