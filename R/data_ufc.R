a34c390b2bfdd52a65c652c731a521c508458f37
Alec L. Robitaille <robit.alec@gmail.com>
2024-03-28 14:46:25 -0300

    hand pair counts

--- R/data_ufc.R
+++ R/data_ufc.R
@@ -3,7 +3,9 @@ data_ufc <- function() {
 
 	DT <- data.table(UFClefties)
 
-	DT[, .(n_fight = .N, n_win = sum(fighter1.win)),
-		 by = .(id = factor(fighter1), is_lefty = factor(fighter1.lefty))]
+	DT[, hand_1 := c('L', 'R')[fighter1.lefty + 1]]
+	DT[, hand_2 := c('L', 'R')[fighter2.lefty + 1]]
+	DT[, hand_pair := factor(paste(hand_1, hand_2, sep = '-'))]
 
+	DT[, .(n_fight = .N, n_win = sum(fighter1.win)), by = hand_pair]
 }
