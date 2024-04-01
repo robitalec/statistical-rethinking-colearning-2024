50235211b17eb414c6050b1439548da717a297ac
Alec L. Robitaille <robit.alec@gmail.com>
2024-04-01 17:44:38 -0300

    swap lr to compare LL LR and RR

--- R/data_ufc.R
+++ R/data_ufc.R
@@ -3,6 +3,14 @@ data_ufc <- function() {
 
 	DT <- data.table(UFClefties)
 
+	DT[fighter1.lefty == 1 & fighter2.lefty == 0,
+		 colnames(DT) := .SD,
+		 .SDcols = stri_replace(
+		 	colnames(DT),
+		 	regex = c('1', '2'),
+		 	replacement = c('2', '1')
+	 	)]
+
 	DT[, hand_1 := c('L', 'R')[fighter1.lefty + 1]]
 	DT[, hand_2 := c('L', 'R')[fighter2.lefty + 1]]
 	DT[, hand_pair := factor(paste(hand_1, hand_2, sep = '-'))]
